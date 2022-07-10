fb5e7849f433281027608527bc30fb98785324b8
zrr <zrexrodriguez@gmail.com>
2022-07-10 17:19:36 -0600

    fix: removed colon after Bearer

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,10 +4,10 @@ import jwt from "jsonwebtoken";
 export default async function verifyToken(req, res, next) {
   //check if token is valid
   const authHeader = req.headers['authorization'];
-  if(!authHeader ||!authHeader.startsWith('Bearer: ')) return res.status(403).send("Error: Invalid token");
+  if(!authHeader ||!authHeader.startsWith('Bearer ')) return res.status(401).send("Error: Invalid header");
   
   //get user
-  const token = authHeader.replace('Bearer: ', '');
+  const token = authHeader.replace('Bearer ', '');
   try{
     req.user = await jwt.verify(token, process.env.SECRETKEY).user;
   }
